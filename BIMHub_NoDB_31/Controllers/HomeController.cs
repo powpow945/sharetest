fbd2bbfe8ab82f710e0954ea5b1c45dc5d7f5509
bnn987654321 <bnn987654321@hotmail.com>
2021-03-19 13:22:43 +0800

    第一次

---
+++ BIMHub_NoDB_31/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Autodesk.Forge;
+using BIMHub_NoDB_31.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BIMHub_NoDB_31.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static dynamic PublicToken { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public Token gettoken()
+        {
+            Token returnobj = new Token();
+
+            TwoLeggedApi oauth = new TwoLeggedApi();
+
+            string grantType = "client_credentials";
+
+            string FORGE_CLIENT_ID = "g0kwQcyAs8TpiFGe5OPq6dbIh20BaKhw";
+            string FORGE_CLIENT_SECRET = "wsK6CJQcs4pEgopw";
+
+            PublicToken = oauth.Authenticate(
+              FORGE_CLIENT_ID,
+              FORGE_CLIENT_SECRET,
+              grantType,
+              new Scope[] { Scope.DataRead, Scope.BucketRead, Scope.AccountRead, Scope.BucketUpdate, Scope.DataWrite });
+
+            JObject jobj = PublicToken.ToJson();
+            returnobj.access_token = jobj["access_token"].ToString();
+            returnobj.expires_in = jobj["expires_in"].ToString();
+
+            return returnobj;
+        }
+    }
+}
---
+++ BIMHub_NoDB_31/Models/Token.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BIMHub_NoDB_31.Models
+{
+    public class Token
+    {
+        public string access_token { get; set; }
+        public string expires_in { get; set; }
+    }
+}
